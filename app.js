3e39ec60c71cec44e62dc7c296aea36605f10558
Osidur <hugo.pollack@elev.ga.ntig.se>
2021-04-27 09:58:08 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,5 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('index.ejs'))
-app.listen(port, () => console.log(`Example app listening on port port!`))
+app.use(express.json())
+app.use(express.urlencoded())
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => res.render('index.ejs'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
