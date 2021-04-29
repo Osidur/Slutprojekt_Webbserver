8d5e4f77f4120371fe9e3a49063d3d687369a283
Osidur <hugo.pollack@elev.ga.ntig.se>
2021-04-29 13:11:58 +0200

    added images and a card. need to move card to center

--- app.js
+++ app.js
@@ -6,5 +6,7 @@ app.use(express.json())
 app.use(express.urlencoded())
 app.set('view engine', 'ejs')
 
+app.use(express.static(__dirname +"\\static\\"))
+
 app.get('/', (req, res) => res.render('home.ejs'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
